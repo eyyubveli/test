679990a9527c31c101c5eeb50bfd92fbf70c22d3
eyyubveli <eyub.050@gmail.com>
2023-06-17 00:11:32 -0700

    refactor code

--- index.js
+++ index.js
@@ -5,16 +5,13 @@ const axios = require("axios");
 const keyboard = new InlineKeyboard().text('MP3', 'mp3').row().text('MP4', 'mp4');
 require("dotenv").config();
 
-const admin = require("firebase-admin");
-const serviceAccount = require("./config.json")
-
-
+ const admin = require("firebase-admin");
+ const serviceAccount = require("./config.json")
 
 admin.initializeApp({
     credential : admin.credential.cert(serviceAccount)
 });
-
-const firestore = admin.firestore();    
+ const firestore = admin.firestore();    
 
 const bot = new Bot(process.env.BOT_TOKEN);
 
@@ -25,21 +22,6 @@ bot.command('start',  (ctx) => {
     return ctx.reply(`Xoş gəldin ${name}. Url daxil et!`);
 });
 
-
-bot.catch((err) => {
-    const ctx = err.ctx;
-    ctx.reply("Bir xəta oldu. Daha sonra yenidən yoxlayın");
-    const e = err.error;
-    if (e instanceof GrammyError) {
-      console.error("Error in request:", e.description);
-    } else if (e instanceof HttpError) {
-      console.error("Could not contact Telegram:", e);
-    } else {
-      console.error("Unknown error:", e);
-    }
-  });
-
-
 async function isTikTokURL(url){
     const pattern = /^(?:https?:\/\/)?(?:www\.)?(?:vt\.)?tiktok\.com\/.*$|^(?:https?:\/\/)?(?:www\.)?vt\.tiktok\.com\/.*$/
 
@@ -47,13 +29,15 @@ async function isTikTokURL(url){
 }
 
 bot.on("message", async (ctx) => {
-    text = ctx.message.text ? ctx.message.text : "bilinməyən bir yazı formatı" ;
+    text = ctx.message?.text  || "bilinməyən bir yazı formatı" ;
     const chatID = ctx.message.chat.id;
-    const username = ctx.message.from.username ? ctx.message.from.username : "username tapılmadı" ;
+    const username = ctx.message?.from.username || "username tapılmadı" ;
     const name = ctx.from.first_name;
-   
+    
     const checkUrl = await isTikTokURL(text);
-
+ 
+    if(!checkUrl) return ctx.reply(`Doğru bir url daxil et`);
+    
     const userRef = firestore.collection('users').doc();
     const timeStamp = admin.firestore.FieldValue.serverTimestamp();
 
@@ -62,15 +46,11 @@ bot.on("message", async (ctx) => {
         name : name,
         chatID : chatID,
         username : username,
-        timestamp : timeStamp
-        
+        timestamp : timeStamp   
     }
 
     await userRef.set(userData);
-    
-    if(!checkUrl) return await ctx.reply(`Doğru bir url daxil et`);
-    
-    
+
     await ctx.reply('Format seçin', {
         reply_markup: keyboard,
     });
@@ -93,35 +73,30 @@ bot.on("message", async (ctx) => {
     return options;
 }
 
-bot.callbackQuery("mp3", async (ctx)=>{
-    try {
-        const loadingMessage = await ctx.reply('Yüklənir...');
+
+bot.callbackQuery(["mp3", "mp4"], async (ctx)=>{
+    const fileType = ctx.callbackQuery.data;
+    const loadingMessage = await ctx.reply('Yüklənir...');
+    await ctx.answerCallbackQuery(`${fileType} seçildi!`);
+    try{
         await ctx.deleteMessage();
         const response = await axios.request(getOptions());
-        const mp3 = await response.data.result.music.url_list[0];
-        console.log(mp3);
-        await ctx.replyWithAudio(new InputFile(new URL(mp3)));
-        await ctx.api.deleteMessage(ctx.chat.id, loadingMessage.message_id)
-      } catch (err) {
+        const fileMP3 = await response.data.result.music.url_list[0];
+        const fileMP4 = await response.data.result.video.url_list[0]; 
+
+        if(fileType === "mp3"){
+            await ctx.replyWithAudio(new InputFile(new URL(fileMP3)));
+        }else if(fileType === "mp4"){
+            await ctx.replyWithVideo(new InputFile(new URL(fileMP4)));
+        }
+        await ctx.api.deleteMessage(ctx.chat.id, loadingMessage.message_id);
+    }catch(err){
         console.log(err);
-          return ctx.reply('Tapa bilmedim yeniden yoxla');
-      }
+        return ctx.reply("İndi tapa bilmədim. Daha sonra yenidən yoxla");
+    }
+
 })
 
-bot.callbackQuery("mp4", async (ctx)=>{
-    try {
-        const loadingMessage = await ctx.reply('Yüklənir...');  
-        await ctx.deleteMessage();
-        const response = await axios.request(getOptions());
-        const mp4 = await response.data.result.video.url_list[0];
-        console.log(mp4);
-        await ctx.replyWithVideo(new InputFile(new URL(mp4)));
-        await ctx.api.deleteMessage(ctx.chat.id, loadingMessage.message_id);
-      } catch (err) {
-          console.log(err);
-          return ctx.reply('Tapa bilmedim yeniden yoxla');
-      }
-})
 
 if (process.env.NODE_ENV === "production") {
   const app = express();
