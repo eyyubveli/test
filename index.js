90a1d6e626db9d57918a82bf9554d0440a894614
eyyubveli <82232335+eyyubveli@users.noreply.github.com>
2023-06-19 01:38:11 +0400

    Update index.js

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ bot.command('start',  async (ctx) => {
     const username = ctx.from?.username || "username qeyd edilməyib";
 
     try {
-        const setUserRef = userRef.doc(ctx.from.id.toString())
+        const setUserRef = userRef.doc(ctx.message.from.id.toString())
         const userDoc = await setUserRef.get();
 
         if(!userDoc.exits){
-            await setUserRef.set({name, chatID, username, timeStamp, isRegistered: true});
+            await setUserRef.set({name, chatID, username, timeStamp});
         }
 
         return ctx.reply(`Xoş gəldin ${name}. Url daxil et!`);
