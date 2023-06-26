185464d892718d7e601a0ca7c3e2bb7483f39a55
eyyubveli <82232335+eyyubveli@users.noreply.github.com>
2023-06-26 06:00:59 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,43 +3,19 @@ const express = require("express");
 const { URL } = require("url");
 const axios = require("axios");
 const keyboard = require("./src/keyboard");
-const {userRef, timeStamp} = require("./src/firebasedb");
 require("dotenv").config();
 
 let text;
 
 const bot = new Bot(process.env.BOT_TOKEN);
 
-async function isUser(userId){
-  const newUserRef = userRef.doc(userId);
-  const doc = await newUserRef.get();
-  return doc.exists;
-}
+
 
 bot.command('start',  async (ctx) => {
     const name = ctx.from.first_name;
-    const chatID = ctx.chat.id;
-    const username = ctx.from?.username || "username qeyd edilməyib";
-    const userId = ctx.message.from.id.toString();
-    const checkUser = await isUser(userId);
+  
+    return ctx.reply(`Xoş gəldin ${name}. Url daxil et!`);
     
-    try {
-        if(!checkUser){
-            const userData = {
-                name,
-                chatID,
-                username,
-                timeStamp
-            }
-          await userRef.doc(userId).set(userData)
-        }
-        
-        return ctx.reply(`Xoş gəldin ${name}. Url daxil et!`);
-      } catch (error) {
-        console.log(error)
-    
-        return ctx.reply('Daha sonra yeniden yoxlayin');
-      }
 
 });
 
